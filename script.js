18ebc31830ff12a3bd733ed4b6caff2a7d5ca27b
anata_mee <108978057+ArdivaAnhar@users.noreply.github.com>
2024-01-29 15:14:42 +0700

    i love you

---
+++ script.js
@@ -0,0 +1,10 @@
+document.title = "Belajar Js";
+
+const panjang = () => {
+  const panjangg = document.getElementById("panjang");
+};
+
+const lebar = () => {
+  const lebarr = document.getElementById("lebar");
+  console.log(lebarr);
+};
